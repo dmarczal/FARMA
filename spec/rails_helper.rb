2753cd8d5f9203862135b8225f5e0239c484e933
Diego Marczal <dmarczal@gmail.com>
2015-12-20 09:59:59 -0200

    setup rspec and factorygirl

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -26,14 +26,17 @@ require 'rspec/rails'
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
-RSpec.configure do |config|
-  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+require 'capybara/rspec'
+require 'simplecov'
+SimpleCov.start
 
-  # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, remove the following line or assign false
-  # instead of true.
-  config.use_transactional_fixtures = true
+RSpec.configure do |config|
+
+  config.include FactoryGirl::Syntax::Methods
+  config.before(:suite) do
+    FactoryGirl.find_definitions
+  end
+
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
