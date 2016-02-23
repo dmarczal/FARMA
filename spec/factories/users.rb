b458c656d31fd84f18f7d879419d9188be4af078
Diego Marczal <dmarczal@gmail.com>
2016-02-23 17:07:56 -0300

    Setup spec files

---
+++ spec/factories/users.rb
@@ -0,0 +1,14 @@
+# This will guess the User class
+FactoryGirl.define do
+  factory :user do
+    name                  { Faker::Internet.name }
+    email                 { Faker::Internet.email }
+    password              { 12345678 }
+    password_confirmation { 12345678 }
+
+    factory :user_actived do
+      confirmed_at Time.now
+    end
+  end
+end
+
