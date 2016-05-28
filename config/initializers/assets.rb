72f4da01c50fc6ffaf22665f0055cd311441b134
Darlan <dla14@live.com>
2016-05-28 03:52:54 -0300

    Customizing the layout of teacher session

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,19 +10,8 @@ Rails.application.config.assets.version = '1.0'
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
 # Rails.application.config.assets.precompile += %w( search.js )
 
-<<<<<<< HEAD
-Rails.application.config.assets.precompile += %w( admin/login.css )
-Rails.application.config.assets.precompile += %w( farma-01.jpg )
-Rails.application.config.assets.precompile += %w( farma-02.png )
-Rails.application.config.assets.precompile += %w( farma-03.jpg )
-Rails.application.config.assets.precompile += %w( farma-04.jpg )
-Rails.application.config.assets.precompile += %w( farma-05.jpg )
-Rails.application.config.assets.precompile += %w( math.png )
-Rails.application.config.assets.precompile += %w( farma-06.jpg )
-Rails.application.config.assets.precompile += %w( farma-07.jpg )
-Rails.application.config.assets.precompile += %w( missing.png )
-Rails.application.config.assets.precompile += %w( favicon.ico )
-=======
+
+
 Rails.application.config.assets.precompile += %w( admin/login.css.scss )
->>>>>>> 1ca46bbd0012223a54285cd2106e65db66875060
+
 Rails.application.config.assets.precompile += %w( ckeditor/*)
