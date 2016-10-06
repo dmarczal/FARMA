287ea446783cd0fcda27cfcc2179e148e4fa9e4b
Diego Marczal <dmarczal@gmail.com>
2016-10-06 20:07:55 -0300

    Added lib/keyboard/partias and templates to assests pipeline

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -14,4 +14,6 @@ Rails.application.config.assets.version = '1.0'
 
 Rails.application.config.assets.precompile += %w( admin/login.css.scss )
 
-Rails.application.config.assets.precompile += %w( ckeditor/*)
+Rails.application.config.assets.precompile += %w( ckeditor/* )
+Rails.application.config.assets.precompile += %w( lib/keyboard/partials )
+Rails.application.config.assets.precompile += %w( lib/keyboard/templates )
