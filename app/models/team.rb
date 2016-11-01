972a2157e6be156cddd502c3cc02b9968805c0f5
Jefferson Henrique Ramos <jeff.hen0811@gmail.com>
2016-11-01 16:00:33 -0200

    link for teacher and student, refactor teams

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,9 +1,13 @@
 class Team < ActiveRecord::Base
-  has_many :users_teams
-  has_many :los_teams
+  has_many :users_teams, dependent: :destroy
+  has_many :los_teams, dependent: :destroy
   has_many :los , through: :los_teams
   has_many :users, through: :users_teams
+  has_many :tips_counts, dependent: :destroy
+  has_many :answers, dependent: :destroy
   belongs_to :user
+
+  validates :name, :code, presence: true
 
   def to_register?(user_id, key)
     if key == code
