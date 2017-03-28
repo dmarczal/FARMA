966f4a8683834df13407c46d810a9e9266ca6acd
Jefferson Henrique Ramos <jeff.hen0811@gmail.com>
2017-03-28 16:49:24 -0300

    rebase routes, applications, enviroments and initializers

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -37,6 +37,11 @@ Rails.application.configure do
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
 
+  # Randomize the order test cases are executed.
+  config.active_support.test_order = :random
+
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 end
