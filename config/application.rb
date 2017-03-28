cd7c705338e8c02b4cf1db7c07a0d24b0d356e9f
Jefferson Henrique Ramos <jeff.hen0811@gmail.com>
2017-03-28 16:15:26 -0300

    update gems and app

--- config/application.rb
+++ config/application.rb
@@ -1,4 +1,4 @@
-require File.expand_path('../boot', __FILE__)
+require_relative 'boot'
 
 require 'rails/all'
 
@@ -11,23 +11,5 @@ module Farma
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
-    config.autoload_paths += %W(#{config.root}/lib/simple_form_extensions)
-    config.autoload_paths += %W(#{config.root}/lib/breadcrumbs_on_rails_extensions)
-    config.autoload_paths += Dir["#{config.root}/lib/**/"]
-    config.assets.precompile += ["codemirror*", "codemirror/**/*"]
-
-    # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
-    # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
-    # config.time_zone = 'Central Time (US & Canada)'
-    config.time_zone = 'Brasilia'
-
-    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    config.i18n.enforce_available_locales = false
-    config.i18n.available_locales = ["pt-BR"]
-    config.i18n.default_locale = :'pt-BR'
-
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
   end
 end
