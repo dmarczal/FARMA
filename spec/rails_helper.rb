876e8b58a8c19f45ac7e323247cc7f0153b1b0e4
Jefferson Ramos <jefferson.ramos@codeminer42.com>
2017-12-30 23:10:54 -0200

    refactor the introduction_actions_spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -18,6 +18,7 @@ RSpec.configure do |config|
 
   config.include Devise::Test::IntegrationHelpers, type: :feature
   config.include ActionView::Helpers::TextHelper, type: :feature
+  config.include Helpers::Form, type: :feature
 
   config.infer_spec_type_from_file_location!
   config.filter_rails_from_backtrace!
