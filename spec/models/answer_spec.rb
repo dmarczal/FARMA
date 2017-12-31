d5fdd1784da0deb2aeb4c59c32831de744b60130
Jefferson Ramos <jefferson.ramos@codeminer42.com>
2017-12-31 19:15:12 -0200

    refactor the answer_spec

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,89 +1,58 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
+  let(:question) { create(:question, correct_answer: '10') }
+  let(:exercise) { create(:exercise, lo: lo) }
+  let(:lo)       { create(:lo, user: user) }
+  let(:user)     { create(:user, :actived) }
 
-  describe 'initialize' do
-    before do
-      @user = FactoryGirl.create(:user_actived)
-      @lo = FactoryGirl.create(:lo, user: @user)
-      exercise = FactoryGirl.create(:exercise, lo: @lo)
-      @question = FactoryGirl.create(:question, exercise: exercise, correct_answer: '10')
-    end
+  describe 'Create the answers' do
+    context 'when only one user responds' do
+      it "it's correct" do
+        answer = question.answers.new user_id: user.id, response: '5'
+        answer.response= 10
+  
+        expect(answer).to be_correct
+      end
 
-    it "The answer must be correct after edit" do
-      answer = @question.answers.new user_id: @user.id, response: '5'
-      answer.response= 10
+      it 'has only one attempt' do
+        answer = question.answers.new user_id: user.id, response: '10'
 
-      expect(answer.correct).to eq true
-    end
-
-    context 'Have one user' do
-
-      it 'the answer must be correct and have one attempt' do
-        answer = @question.answers.new user_id: @user.id, response: '10'
-
-        expect(answer.correct).to eq true
         expect(answer.attempt_number).to eq 1
       end
 
-      it 'the answer must be correct and have two attempt' do
-        answer1 = @question.answers.new user_id: @user.id, response: '4'
-        answer1.save
+      it 'has two attempts' do
+        question.answers.create user_id: user.id, response: '4'
+        answer = question.answers.new user_id: user.id, response: '10'
 
-        answer2 = @question.answers.new user_id: @user.id, response: '10'
-
-        expect(answer2.correct).to eq true
-        expect(answer2.attempt_number).to eq 2
-      end
-
-      it 'the answer must be correct and have two attempt' do
-        answer1 = @question.answers.new user_id: @user.id, response: '4'
-        answer1.save
-
-        answer2 = @question.answers.new user_id: @user.id, response: '5'
-        answer2.save
-
-        answer3 = @question.answers.new user_id: @user.id, response: '8'
-
-        expect(answer3.correct).to eq false
-        expect(answer3.attempt_number).to eq 3
+        expect(answer.attempt_number).to eq 2
       end
     end
 
-    context 'It has more than one user' do
-      before do
-        @user2 = FactoryGirl.create(:user_actived)
-      end
+    context "when it's more than one user responding" do
+      let(:user2) { create(:user, :actived) }
 
-      it 'the answer must be correct and have one attempt' do
-        answer1 = @question.answers.new user_id: @user2.id, response: '10'
-        answer1.save
-        answer2 = @question.answers.new user_id: @user.id, response: '10'
+      it 'has only one attempt' do
+        question.answers.create user_id: user2.id, response: '10'
+        answer2 = question.answers.new user_id: user.id, response: '10'
 
-        expect(answer2.correct).to eq true
         expect(answer2.attempt_number).to eq 1
       end
     end
 
-    context 'Have teams' do
-      before do
-        @user2 = FactoryGirl.create(:user_actived)
-        @team = FactoryGirl.create(:team, user: @user)
-        user_team = @team.users_teams.new user_id: @user2.id
-        user_team.save
+    context 'when has team' do
+      let(:user2) { create(:user, :actived) }
+      let(:team) { create(:team, user: user) }
 
-        lo_team = @team.los_teams.new lo_id: @lo.id
-        lo_team.save
+      before do
+        team.users << user2
+        team.los << lo
       end
 
-      it 'responses must be separated by teams' do
-        answer1 = @question.answers.new user_id: @user.id, response: '3'
-        answer1.save
-
-        answer2 = @question.answers.new user_id: @user.id, response: '5', team_id: @team.id
-        answer2.save
-
-        answer3 = @question.answers.new user_id: @user.id, response: '10', team_id: @team.id
+      it 'responses are separated by teams' do
+        answer1 = question.answers.create user_id: user.id, response: '3'
+        answer2 = question.answers.create user_id: user.id, response: '5', team_id: team.id
+        answer3 = question.answers.new user_id: user.id, response: '10', team_id: team.id
 
         expect(answer1.attempt_number).to eq 1
         expect(answer2.attempt_number).to eq 1
