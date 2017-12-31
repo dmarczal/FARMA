2ea2e0c3a3afc4fd13b2c8bc87f474be1bf49b39
Jefferson Ramos <jefferson.ramos@codeminer42.com>
2017-12-31 17:42:02 -0200

    refactor the comparison_spec

--- spec/math_comparison/comparison_spec.rb
+++ spec/math_comparison/comparison_spec.rb
@@ -1,82 +1,45 @@
 require 'rails_helper'
 require 'math_comparison/comparison'
 
-describe 'the class comparison' do
+describe MathComparison::Comparison do
+  context "when it's just an answer" do
+    subject { MathComparison::Comparison.new correct_answer: '10', 
+                                             precision:       1, 
+                                             cmas_order:      false }
 
-  describe 'only one answer' do
-    before :each do
-      @comparison = MathComparison::Comparison.new(correct_answer: '10', precision: 1, cmas_order: false)
-    end
-
-    it 'the response must be correct' do
-      expect(@comparison.right_response?('10')).to eq true
-    end
-
-    it 'the response must not be correct' do
-      expect(@comparison.right_response?('5')).to eq false
-    end
-
-    it 'the sum must be correct' do
-      expect(@comparison.right_response?('5 + 5')).to eq true
-    end
-
-    it 'the sum must not be correct' do
-      expect(@comparison.right_response?('5 + 3')).to eq false
-    end
-
-    it 'the square root must be correct' do
-      expect(@comparison.right_response?('sqrt(100)')).to eq true
-    end
-
-    it 'the square root must not be correct' do
-      expect(@comparison.right_response?('sqrt(49)')).to eq false
-    end
+    it { is_expected.to be_right_response '10' }
+    it { is_expected.to_not be_right_response '5' }
+    it { is_expected.to be_right_response '5 + 5' }
+    it { is_expected.to_not be_right_response '5 + 3' }
+    it { is_expected.to be_right_response 'sqrt(100)' }
+    it { is_expected.to_not be_right_response 'sqrt(49)' }
   end
 
-  describe 'more than one answer and need not be in the same order' do
-    before :each do
-      @comparison = MathComparison::Comparison.new(correct_answer: '10;20', precision: 1, cmas_order: false)
-    end
-
-    it 'the answer must be correct' do
-      expect(@comparison.right_response?('10;20')).to eq true
-    end
-
-    it 'the answer must not be correct' do
-      expect(@comparison.right_response?('20;10')).to eq true
-    end
+  context "when it's more than one answer" do
+    subject { MathComparison::Comparison.new correct_answer: '10;20', 
+                                             precision:       1, 
+                                             cmas_order:      false }
+    
+    it { is_expected.to be_right_response '10;20' }
+    it { is_expected.to be_right_response '20;10' }
   end
 
-  describe 'more than one answer and needs to be in the same order' do
-    before :each do
-      @comparison = MathComparison::Comparison.new(correct_answer: '10;20', precision: 1, cmas_order: true)
-    end
+  context "when it's more than one answer and in the same order" do
+    subject { MathComparison::Comparison.new correct_answer: '10;20', 
+                                             precision:       1, 
+                                             cmas_order:      true }
 
-    it 'the answer must be correct' do
-      expect(@comparison.right_response?('10;20')).to eq true
-    end
-
-    it 'the answer must not be correct' do
-      expect(@comparison.right_response?('20;10')).to eq false
-    end
+    it { is_expected.to be_right_response '10;20' }
+    it { is_expected.to_not be_right_response '20;10' }
   end
 
-  describe 'with three decimal places of precision' do
-    before :each do
-      @comparison = MathComparison::Comparison.new(correct_answer: '2.5684952', precision: 3, cmas_order: false)
-    end
+  context 'when it has three decimal places of precision' do
+    subject { MathComparison::Comparison.new correct_answer: '2.5684952', 
+                                             precision:       3, 
+                                             cmas_order:      false }
 
-    it 'the answer must be correct' do
-      expect(@comparison.right_response?('2.568')).to eq true
-    end
-
-    it 'the answer must not be correct' do
-      expect(@comparison.right_response?('2.56')).to eq false
-    end
-
-    it 'the answer must not be correct' do
-      expect(@comparison.right_response?('2.5684952')).to eq true
-    end
+    it { is_expected.to be_right_response '2.568' }
+    it { is_expected.to_not be_right_response '2.56' }
+    it { is_expected.to be_right_response '2.5684952' }
   end
-
 end
