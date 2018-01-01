6f65bd3ece9c827c99ad6fb2abde64f379774e3f
Jefferson Ramos <jefferson.ramos@codeminer42.com>
2018-01-01 12:53:08 -0200

    add temping gem

--- Gemfile
+++ Gemfile
@@ -58,6 +58,7 @@ end
 
 group :test do
   gem 'shoulda-matchers', '~> 3.1'
+  gem 'temping'
 end
 
 group :production do
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -8,5 +8,9 @@ RSpec.configure do |config|
     mocks.verify_partial_doubles = true
   end
 
+  config.after do
+    Temping.teardown
+  end
+
   config.shared_context_metadata_behavior = :apply_to_host_groups
 end
