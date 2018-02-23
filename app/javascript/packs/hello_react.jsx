307716169acd3581db602c2561241f997db721ce
Jefferson Ramos <jeff.hen0811@gmail.com>
2018-02-22 21:02:16 -0300

    configure the react on rails

--- app/javascript/packs/hello_react.jsx
+++ app/javascript/packs/hello_react.jsx
@@ -6,8 +6,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 
-const Hello = props => (
-  <div>Hello {props.name}!</div>
+const Hello = ({ name }) => (
+  <div>Hello {name}!</div>
 )
 
 Hello.defaultProps = {
