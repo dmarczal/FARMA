d0b395049792a591a4880f29ccb61ea6b9aabf6a
Aguinaldo Goudinho <aguinaldogoldinho@gmail.com>
2018-03-26 09:21:41 -0300

    refactoring the search page

--- app/models/admin/research.rb
+++ app/models/admin/research.rb
@@ -1,2 +1,13 @@
-class Admin::Research < ActiveRecord::Base
+class Admin::Research < ApplicationRecord
+    validates :title, :ref, :abstract, :link, :kind, presence: true
 end
+
+#create_table "admin_researches", id: :serial, force: :cascade do |t|
+#  t.string "title"
+#  t.text "ref"
+#  t.text "abstract"
+#  t.string "link"
+#  t.string "kind"
+#  t.datetime "created_at", null: false
+#  t.datetime "updated_at", null: false
+#end
--- app/views/admin/researches/index.json.jbuilder
+++
@@ -1,4 +0,0 @@
-json.array!(@admin_researches) do |admin_research|
-  json.extract! admin_research, :id, :title, :ref, :abstract, :link, :kind
-  json.url admin_research_url(admin_research, format: :json)
-end
