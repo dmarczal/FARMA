84fff5b2a603f8c7968c29962abbe2711045655e
Diego Marczal <dmarczal@gmail.com>
2021-04-08 21:00:58 -0300

    Dockerize the application

--- config/application.rb
+++ config/application.rb
@@ -25,5 +25,7 @@ module Farma
     config.i18n.enforce_available_locales = false
     config.i18n.available_locales = ["pt-BR"]
     config.i18n.default_locale = :'pt-BR'
+
+    ENV.update YAML.load_file('config/application.yml')[Rails.env]
   end
 end
