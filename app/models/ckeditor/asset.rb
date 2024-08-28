90be2353a3f857408ee0e28da2dda5614a0b4a93
Diego Marczal <dmarczal@gmail.com>
2024-08-28 18:01:27 -0300

    Updates to run the app

--- app/models/ckeditor/asset.rb
+++ app/models/ckeditor/asset.rb
@@ -1,4 +1,4 @@
-class Ckeditor::Asset < ActiveRecord::Base
-  include Ckeditor::Orm::ActiveRecord::AssetBase
-  include Ckeditor::Backend::Paperclip
-end
+# class Ckeditor::Asset < ActiveRecord::Base
+#   include Ckeditor::Orm::ActiveRecord::AssetBase
+#   include Ckeditor::Backend::Paperclip
+# end
